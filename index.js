9c5ccde824057036b6cf79d7ac3f546ffe68511b
sanjeetjha-hub <1999sanjeetjha@gmai.com>
2023-04-10 09:36:37 +0530

    Implemented Maze Algorithm

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse   } = Matt
 const engine = Engine.create();
 const { world } = engine;
 
-const cells = 5;
-const width = 1000;
-const height = 1000;
+const cells = 3;
+const width = 600;
+const height = 600;
 
 const render = Render.create({
   element: document.body,
@@ -29,10 +29,79 @@ const walls = [Bodies.rectangle(width/2, 0, width, 40, {isStatic : true}),
     Bodies.rectangle(width, height/2, 40, height, {isStatic : true})]
 World.add(world, walls);
 
+//Maze Generation
+
+const shuffle = (arr) => {
+    let counter = arr.length;
+   
+    while(counter>0){
+        const index = Math.floor(Math.random() * counter)
+
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] =  temp;
+    }
+    return arr;
+};
+
 const grid = Array(cells).fill(null).map(()=> Array(cells).fill(false));
-
 const verticals = Array(cells).fill(null).map(()=> Array(cells-1).fill(false));
-
 const horizontals = Array(cells-1).fill(null).map(()=> Array(cells).fill(false));
 
-console.log(grid,verticals,horizontals);
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+
+const stepThroughCell = (row,column) => {
+    //If i have visited the cell at [row,column] then return 
+    if(grid[row][column]){
+        return; 
+    }
+
+    //Mark this cell as being visted
+    grid[row][column] = true;
+
+    //Assemble randomly orderded list of neighbours
+    const neighbours = shuffle([
+        [row -1, column,'up'],
+        [row, column + 1,'right'],
+        [row+1, column,'down'],
+        [row, column-1,'left'],
+    ]);
+
+    
+    //for each neighbour
+    for(let neighbour of neighbours){
+        const [nextRow,nextColumn,directions] = neighbour;
+
+    //see if that neighbour is out of bound
+        if(nextRow< 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells)
+        {
+            continue;
+        }
+    //if we have visited the neighbour, continue to next neighbour
+    if(grid[nextRow][nextColumn])
+    {
+        continue;
+    }
+    //remove a wall from either vertical or horizontal
+    if(directions === 'left'){
+        verticals[row][column-1] = true;
+    }
+    else if(directions === 'right'){
+        verticals[row][column] = true;
+    }else if (directions === 'up'){
+        horizontals[row-1][column]= true;
+    }else if(directions==='down'){
+        horizontals[row][column]=true;
+    }
+    
+    stepThroughCell(nextRow,nextColumn);
+    }
+    //visit the next cell
+    
+};
+console.log(horizontals,verticals);
+stepThroughCell(1,1);
